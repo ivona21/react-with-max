3830b731b07d8bcb83345ee984f2fb1049d45046
Ivona <ivonamalekovic@hotmail.com>
2018-03-21 22:27:17 +0100

    displaying list dynamically

--- src/App.js
+++ src/App.js
@@ -51,19 +51,16 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this, "Iiiiiii")}
-          change={this.nameChangedHandler} >
-            My Hobbies: Programming
-        </Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age} />
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return ( 
+            <Person 
+              name={person.name} 
+              age={person.age}
+              click={this.switchNameHandler.bind(this, "Iiiiiii")}
+              change={this.nameChangedHandler} >
+                My Hobbies: Programming
+            </Person>)
+          })}
       </div>           
       )
     }

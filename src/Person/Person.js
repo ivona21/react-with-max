1a99eced69c63210899e9bc76968d4a9f9be20da
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-28 10:43:47 +0200

    error boundary for unpredictable errors

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./Person.css";
 
 const person = (props) => {   
-    return (
+    const rnd = Math.random();
+
+    console.log(rnd);
+    if (rnd > 0.7) {
+        throw new Error("Something wend wrong!");
+    }
+
+    return (       
         <div className={styles.Person}>
              <h5 onClick={props.click}>I'm {props.name} and I am {props.age} years old!</h5>
              <h6>{props.children}</h6>

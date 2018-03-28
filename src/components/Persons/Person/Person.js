fc88e69ae3dfa1903334822f3cc36eec0fab2b4c
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-28 14:22:12 +0200

    converting stateless to stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import styles from "./Person.css";
 
-const person = (props) => {   
-    return (       
-        <div className={styles.Person}>
-             <h5 onClick={props.click}>I'm {props.name} and I am {props.age} years old!</h5>
-             <h6>{props.children}</h6>
-             <input type="text" onChange={props.change} value={props.name} />
-        </div>
-       
-    )
+class Person extends Component {
+    render() {
+        return (       
+            <div className={styles.Person}>
+                 <h5 onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</h5>
+                 <h6>{this.props.children}</h6>
+                 <input type="text" onChange={this.props.change} value={this.props.name} />
+            </div>
+           
+        )
+    }
 }
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "./Person/Person";
 
-const persons = (props) => props.persons.map((person, index) => {
-        return <Person
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              click={() => props.clicked(index)}                  
-              change={(event) => props.changed(event, person.id)}
-            />
-      });
-  
-export default persons;
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return <Person
+        name={person.name}
+        age={person.age}
+        key={person.id}
+        click={() => this.props.clicked(index)}
+        change={(event) => this.props.changed(event, person.id)}
+      />
+    });
+  }
+}
+
+export default Persons;
